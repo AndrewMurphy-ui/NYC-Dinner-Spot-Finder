ca52692cd1ed62cbe9c93e5ace2bb45601b98cea
Andrew Murphy <legionastartesofficial@gmail.com>
2025-04-25 09:34:57 +0900

    Add files via upload

---
+++ new_york_dinner_spots.sql
@@ -0,0 +1,51 @@
+select *
+from nomnom;
+
+SELECT distinct neighborhood
+from nomnom;
+
+
+select distinct cuisine
+from nomnom;
+
+select *
+from nomnom
+where cuisine = 'Chinese';
+
+SELECT *
+from nomnom
+where review >= 4;
+
+select *
+from nomnom
+where cuisine = 'Italian'
+and price = '$$$;'
+
+SELECT *
+FROM nomnom
+where name like '%meatball%';
+
+SELECT *
+from nomnom
+where neiborhood = 'Midtown'
+or neighborhood = 'Downtown'
+or neighborhood = 'Chinatown';
+
+Select *
+from nomnom
+where health IS null;
+
+select *
+from nomnom 
+order by review desc 
+limit 10;
+
+SELECT name,
+ CASE
+  WHEN review > 4.5 THEN 'Extraordinary'
+  WHEN review > 4 THEN 'Excellent'
+  WHEN review > 3 THEN 'Good'
+  WHEN review > 2 THEN 'Fair'
+  ELSE 'Poor'
+ END AS 'Review'
+FROM nomnom;
